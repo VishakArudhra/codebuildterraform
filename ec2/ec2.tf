00c1096d161feff01623f7f84002a82a85eafe97
maheshp3kt <94041832+maheshp3kt@users.noreply.github.com>
2022-08-22 16:16:24 +0530

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -6,11 +6,7 @@ terraform {
    region  = "ap-south-1"
  }
 }
-provider "aws" {
-  access_key = "AKIAYQTPUXJFTF4PGMPP"
-  secret_key = "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"
-  region     = "ap-south-1"
-}
+
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "terraform.tf-bucket14"
